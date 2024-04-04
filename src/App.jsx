cd4b6b980c9b28dea08500dc612d0563140270b2
Dilmani Kiriella <it22324856@my.sliit.lk>
2024-04-04 21:12:03 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { BrowserRouter, Routes, Router, Route } from "react-router-dom";
+import DeliveryManagement from "./pages/DeliveryManagement/DeliveryManagement";
+import DeliveryTaskTable from "./pages/DeliveryManagement/DeliveryTaskTable";
+import DeliveryTaskcreateForm from "./pages/DeliveryManagement/DeliveryTaskcreateForm";
+import DeliveryTaskUpdateForm from "./pages/DeliveryManagement/DeliveryTaskUpdateForm";
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<DeliveryManagement />} />
+      <Route path="/Taskpage" element={<DeliveryTaskTable />} />
+      <Route path="/Create" element={<DeliveryTaskcreateForm />} />
+      <Route path="/Update" element={<DeliveryTaskUpdateForm />} />
+      
+    </Routes>
+    </BrowserRouter>
   )
 }
